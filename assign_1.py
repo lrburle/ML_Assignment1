122733e9c888a2e3f741865a01bbe9a14e6832fd
landon <landonburleson203@gmail.com>
2022-09-07 20:17:49 -0500

    Initial commit.

---
+++ assign_1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+class assign1:
+        def __init__(self):
+                self.x_test = np.load('x_test.npy')
+                self.x_train = np.load('x_train.npy')
+                self.y_test = np.load('y_test.npy')
+                self.y_train = np.load('y_train.npy')
+
+        def problem1(self):
+
+        def problem2(self):
+
+        def problem3(self):
+
+if __name__ == '__main__':
+        assignment1 = assign1()
+
+        assignment1.problem1()
+        assignment1.problem2()
+        assignment1.problem3()
