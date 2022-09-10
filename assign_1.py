1188741404381eddfb0e19a281b343b5bd0b3ecf
landon <landonburleson203@gmail.com>
2022-09-09 21:42:08 -0500

    Made slight changes to the repo to better represent the appropriate equations to be used for the problems in the assignment.

--- assign_1.py
+++ assign_1.py
@@ -9,6 +9,34 @@ class assign1:
                 self.y_test = np.load('y_test.npy')
                 self.y_train = np.load('y_train.npy')
 
+                self.w = np.random.rand(1,10)
+                self.a = 0.3 #Learning rate
+
+        def plotGraph(self, xdata, ydata):
+                plt.figure()
+                plt.plot(xdata, ydata)
+                plt.xlabel('Iterations')
+                plt.ylabel('Magnitude')
+                plt.title('Magnitude vs Iterations')
+                plt.show()
+                plt.savefig()
+        
+        def hypothesis(self, xdata):
+                for i in range(len(self.w)):
+                        self.h = self.w[i] * xdata
+
+        def costFunction(self, xdata, ydata):
+                for i in range(len(self.w)):
+                        self.costf = 0.5 * (self.h[i]*(xdata)-ydata)**2
+
+        def gradientDescent(self,xdata, ydata):
+                for j in range(len(self.w)):
+                        self.w[j] = self.w[j] + self.a * (ydata[j] - self.h[j]) * xdata[j]
+        
+        def baseFunctions(self, xdata, sel):
+                
+        
+
         def problem1(self):
 
         def problem2(self):
@@ -16,8 +44,8 @@ class assign1:
         def problem3(self):
 
 if __name__ == '__main__':
-        assignment1 = assign1()
+        a1 = assign1()
 
-        assignment1.problem1()
-        assignment1.problem2()
-        assignment1.problem3()
+        a1.problem1()
+        a1.problem2()
+        a1.problem3()
