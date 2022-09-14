1e1c42c42a1f6a56d0eb3a85f761ff57ca948514
Landon Burleson <landonburleson203@gmail.com>
2022-09-14 15:32:48 -0500

    Made changes throughout. Gradient Descent seems to be working appropriately.

--- assign_1.py
+++ assign_1.py
@@ -16,14 +16,12 @@ class assign1:
         def init_theta(self, m, n):
                 self.theta = np.random.random((m, n))
 
-        def plotGraph(self, xdata, ydata, title, xlabel, ylabel, fileN):
+        def plotGraph(self, xdata, ydata, title, xlabel, ylabel):
                 plt.figure(figsize=[15, 12])
-                plt.plot(xdata, ydata, 'o')
                 plt.xlabel(xlabel, fontsize=18)
                 plt.ylabel(ylabel, fontsize=18)
                 plt.title(title, fontsize=24)
-                plt.savefig(fileN)
-                plt.show()
+                plt.plot(xdata, ydata, 'o')
         
         def hypothesis(self, xdata):
                 return np.dot(self.theta, xdata)
@@ -31,14 +29,11 @@ class assign1:
         def costFunction(self, xdata, ydata):
                 self.costf = 0
                 for i in range(len(self.theta)):
-                        self.costf +=  (self.hypothesis(xdata)-ydata)**2
+                        self.costf += (self.hypothesis(xdata)-ydata)**2
                 self.costf = 0.5 * self.costf
 
-        def gradientDescent(self,xdata, ydata):
-                for j in range((self.theta).size):
-                        error = np.abs((ydata - self.hypothesis(xdata))) / np.abs(ydata)
-                        self.theta[j] = self.theta[j] + self.a * (ydata - self.hypothesis(xdata)) * xdata
-                        return error
+        def gradientDescent(self, xdata, ydata, j):
+                self.theta[0, j] = self.theta[0, j] + self.a * (ydata - self.hypothesis(xdata)) * xdata[j]
 
         def sigmoid(self, data):
                 [x,y] = data.shape
@@ -46,13 +41,13 @@ class assign1:
                 out[:,0] = np.exp(((data[:, 0] - self.mu) ** 2) / (2*self.phi**2)) 
                 return np.concatenate((data, out), axis=1)
 
-        def polynomial_basis(self, data, order):
+        def polynomialBasis(self, data, order):
                 [x,y] = data.shape
                 out = np.zeros((x, 1))
                 out[:,0] = data[:, 0] ** order
                 return np.concatenate((data, out), axis=1) 
         
-        def csv_parse(self, csvFile):
+        def csvParse(self, csvFile):
                 file = open(csvFile, 'rb')
                 file = file.readlines()[1:]
                 data = np.loadtxt(file, delimiter=',')
@@ -66,17 +61,17 @@ class assign1:
                 
                 return x_train, y_train, x_test, y_test
         
-        def prediction_error(self, actual, predicted):
+        def predictionError(self, actual, predicted):
                 return (np.abs(actual - predicted) / np.abs(actual))
 
-        def local_weight(self, query, tao):
+        def localWeight(self, query, tao):
                 [m, n] = self.x_train.shape
                 self.w = np.zeros((m, 1))
                 
                 for i in range(m):
                         self.w[i] = np.exp(-(self.x_train[i] - query)**2 / 2*tao**2)
 
-        def cost_function_LR(self):
+        def costFunctionLR(self):
                 [m, n] = self.w.shape
 
                 for i in range(m):
@@ -87,42 +82,40 @@ class assign1:
 if __name__ == '__main__':
         a1 = assign1()
 
-        a1.plotGraph(a1.x_train, a1.y_train, 'Initial Test', 'x', 'y', 'init.png')
+        a1.plotGraph(a1.x_train, a1.y_train, 'Initial Test', 'x', 'y')
 
         #Training the model for assignment 1
-        a1.init_theta(1, 1)
-
         plt.figure()
-        for i in range(len(a1.x_train)):
-                predictionError = a1.gradientDescent(a1.x_train[i], a1.y_train[i])
-                # plt.scatter(i, predictionError)
-                # plt.pause(0.005)
-        
-        # plt.show()
 
-        a1.x_train = a1.polynomial_basis(a1.x_train, 2) #Input data with the order desired to be concatenated into the original dataset.
-        a1.x_train = a1.sigmoid(a1.x_train) #Input data with the order desired to be concatenated into the original dataset.
+        a1.x_train = a1.polynomialBasis(a1.x_train, 2) #Input data with the order desired to be concatenated into the original dataset.
+        a1.x_train = a1.polynomialBasis(a1.x_train, 3) #Input data with the order desired to be concatenated into the original dataset.
 
+        [n, m] = a1.x_train.shape
 
+        a1.init_theta(1, m)
+
+        for j in range(m):
+                for i in range(n):
+                        a1.gradientDescent(a1.x_train[i], a1.y_train[i], j)
 
         #Testing the model for Question 1
+        a1.x_test = a1.polynomialBasis(a1.x_test, 2) #Input data with the order desired to be concatenated into the original dataset.
+        a1.x_test = a1.polynomialBasis(a1.x_test, 3) #Input data with the order desired to be concatenated into the original dataset.
         e = []
 
-        for i in range(len(a1.x_test)):
+        for i in range(n):
                 y = a1.hypothesis(a1.x_test[i])
-                e[i] = a1.prediction_error(a1.y_test[i], y)
+                e.append(a1.predictionError(a1.y_test[i], y))
         
-        x = np.linspace(1, len(a1.x_test))
+        x = np.linspace(1, n)
 
-        a1.plotGraph(x, e, 'Prediction Error', 'Point Number', 'Prediction Error', 'prediction_error.png')
-
-
+        a1.plotGraph(x, e, 'Prediction Error', 'Point Number', 'Prediction Error')
+        plt.savefig('TestRun.png')
+        plt.show()
 
         #Question 2
  
- 
- 
 	#Updating the data for Question 2 from the CSV file. 
-        [a1.x_train, a1.y_train, a1.x_test, a1.y_test] = a1.csv_parse('Assignment1_Q2_Data.csv')
+        [a1.x_train, a1.y_train, a1.x_test, a1.y_test] = a1.csvParse('Assignment1_Q2_Data.csv')
 
         #Question 3
